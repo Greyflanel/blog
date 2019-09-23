35caf37b9b52c756cad85ed964f9478c1a93992c
Greyflanel <richardlovelace811@gmail.com>
2019-09-23 03:00:18 -0500

    refactored to use lodash _.chain. App is complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,19 @@
+import _ from "lodash"
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => async dispatch => {
+
+/* Whenever we call an action creator from inside of an action creator, we must make sure that we dispatch the result of the action creator and not the creator itself.*/
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+ 
+ _.chain(getState().posts)
+ .map('userId')
+ .uniq()
+ .forEach(id => dispatch(fetchUser(id)))
+ .value()
+};
+
+export const fetchPosts = () => async (dispatch, getState) => {
   const response = await jsonPlaceholder.get("/posts");
 
   dispatch({ type: "FETCH_POSTS", payload: response.data });
@@ -13,3 +26,13 @@ export const fetchUser = (id) => async dispatch => {
 };
   
 
+
+// export const fetchUser = (id) => dispatch => {
+//   _fetchUser(id, dispatch);
+//   };
+// /* One-time Memoization using lodash, to avoid multiple Network calls for the same data.*/
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
